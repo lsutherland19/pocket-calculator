d6befa0a9a92d4cd183e9efbc89e6d63bad9d24d
Ljhauren Sutherland <ljhauren@gmail.com>
2019-01-03 10:48:38 -0500

    finished formatting for now, starting functions

---
+++ pocket-calculator.js
@@ -0,0 +1,75 @@
+function clear (){
+
+}
+
+function negation (){
+
+}
+
+function percentage (){
+
+}
+
+function division (){
+
+}
+
+function seven (){
+
+}
+
+function eight (){
+
+}
+
+function nine (){
+
+}
+
+function multiply (){
+
+}
+
+function four (){
+
+}
+
+function five (){
+
+}
+
+function six (){
+
+}
+
+function subtract (){
+
+}
+
+function one (){
+
+}
+
+function two (){
+
+}
+
+function three (){
+
+}
+
+function addition (){
+
+}
+
+function zero (){
+
+}
+
+function decimal (){
+
+}
+
+function equal (){
+
+}
