63d1dd6fd91bafbbab228d26ab42649045ac8c45
Ljhauren Sutherland <ljhauren@gmail.com>
2019-01-11 10:50:24 -0500

    :(

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -1,3 +1,12 @@
+function screen () {
+  const display = getElementById("screen").value=;
+
+}
+
+function number () {
+  
+}
+
 function clear (){
 
 }
@@ -14,31 +23,7 @@ function division (){
 
 }
 
-function seven (){
-  
-}
-
-function eight (){
-
-}
-
-function nine (){
-
-}
-
 function multiply (){
-
-}
-
-function four (){
-
-}
-
-function five (){
-
-}
-
-function six (){
 
 }
 
@@ -46,23 +31,7 @@ function subtract (){
 
 }
 
-function one (){
-
-}
-
-function two (){
-
-}
-
-function three (){
-
-}
-
 function addition (){
-
-}
-
-function zero (){
 
 }
 
